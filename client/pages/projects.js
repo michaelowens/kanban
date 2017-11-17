55eeb434cf04674297d44ccd1bed0defc8ceef01
Michael Owens <michaelowensnl@gmail.com>
2017-11-17 16:02:55 +0100

    totally did not forget files

---
+++ client/pages/projects.js
@@ -0,0 +1,60 @@
+import comp from '../comp.js'
+import Navbar from '../components/Navbar.js'
+import Sidenav from '../components/Sidenav.js'
+
+export default comp({
+  templateURL: '/client/view/projects.html',
+
+  components: {Navbar, Sidenav},
+
+  data: () => {
+    return {
+      installed: null,
+      projects: [],
+      showAddProject: false,
+      newProject: {
+        name: ''
+      }
+    }
+  },
+
+  methods: {
+    openAddProject (e) {
+      e.preventDefault()
+      this.showAddProject = true
+    },
+
+    addProject (e) {
+      e.preventDefault()
+
+      this.$fetch.postJson('/api/project/add', this.newProject)
+        .then(response => {
+          console.log('addProject: added!', response)
+          this.newProject.name = ''
+          this.showAddProject = false
+
+          this.getProjects()
+        })
+        .catch(response => {
+          console.log('addProject: error!', response)
+        })
+    },
+
+    getProjects () {
+      this.$fetch.json('/api/project/list')
+        .then(projects => this.projects = projects)
+    }
+  },
+
+  created () {
+    this.$fetch.json('/api/installed')
+      .then(installed => {
+        this.installed = installed
+        if (!installed) {
+          this.$router.push('/setup')
+        }
+      })
+
+    this.getProjects()
+  }
+})
